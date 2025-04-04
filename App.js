f5d3da869d6d60ecffad9bef9e571e6119b34e83
Deniz Denizer <ddenizer14@gmail.com>
2025-04-04 18:11:17 +0300

    delete task

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   View,
   Text,
@@ -7,51 +7,80 @@ import {
   Platform,
   StyleSheet,
   StatusBar,
-  Touchable,
-  TouchableOpacity
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  Keyboard
 } from 'react-native';
 
 import Task from './components/Task';
 
 export default function App() {
+
+  const [task, setTask] = useState();
+
+  const [taskItems, setTaskItems] = useState([]);
+
+  const handleAddTask = () => {
+    //when clicked to input field, keyboard will come up
+    Keyboard.dismiss();
+      
+      setTaskItems([...taskItems, task]);
+      setTask(null);
+  }
+
+  const completeTask = (index) => {
+
+    let itemsCopy = [...taskItems];
+    
+    //remove one item in the array and store in this copy array
+    itemsCopy.splice(index,1);
+    setTaskItems(itemsCopy);
+
+  }
+
   return (
-    <View style={styles.container}>
-      {/* <Text> To Do List </Text>
-      <StatusBar style="auto" /> */}
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={styles.container}>
 
-      {/* Todays Tasks */}
+        {/* Todays Tasks */}
 
-      <View style={styles.taskWrapper}>
-        <Text style={styles.sectionTitle}>
-          Today's Tasks
-        </Text>
+        <View style={styles.taskWrapper}>
+          <Text style={styles.sectionTitle}>
+            Today's Tasks
+          </Text>
 
-        <View style={styles.items}>
-          {/* this is where the tasks will go! */}
-          <Task text={'Task 1'}></Task>
-          <Task text={'Task 2'}></Task>
+          <View style={styles.items}>
+            {/* this is where the tasks will go! */}
 
+            {
+              taskItems.map((item, index) => {
+                return (
+                <TouchableOpacity key={index} onPress={() => completeTask(index)}>
+                  <Task text={item} />
+                </TouchableOpacity>)
+              })
+            }
+
+          </View>
         </View>
 
-      
-        
+          {/* Create a new todo list */}
 
+        <KeyboardAvoidingView
+          behavior={Platform.OS === "ios" ? "padding" : "height"}
+          style={styles.writeTaskWrapper}>
+            
+            <TextInput style={styles.input} placeholder={'Create a new todo'} value={task} onChangeText={ text => setTask(text)}/>
+            
+            <TouchableOpacity onPress={ () => handleAddTask()}>
+              <View style={styles.addWrapper}>
+                <Text style={styles.addText}>+</Text>
+              </View>
+            </TouchableOpacity>
+          
+          </KeyboardAvoidingView>
       </View>
-        {/* Create a new todo list */}
-      <KeyboardAvoidingView
-        behavior={Platform.OS === "ios" ? "padding" : "height"}
-        style={styles.writeTaskWrapper}>
-          
-          <TextInput style={styles.input} placeholder={'Create a new todo'}/>
-          
-          <TouchableOpacity>
-            <View style={styles.addWrapper}>
-              <Text style={styles.addText}>+</Text>
-            </View>
-          </TouchableOpacity>
-        
-        </KeyboardAvoidingView>
-    </View>
+    </TouchableWithoutFeedback>
   );
 }
 
